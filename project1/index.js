4abb50af4f2869c9c7aad31d1ba7e1de4bc64d4e
Lawrence <larry@Homecoming.attlocal.net>
2020-09-29 14:22:33 -0400

    messing around with test components

--- project1/index.js
+++ project1/index.js
@@ -1,7 +1,17 @@
+const TestComponent = Vue.component('TestComponent', {
+    name: 'TestComponent',
+    template: '#test-component',
+    props: ['title'],
+  });
+
 // The Vue instance
 let app = new Vue({
     // Options
     el: '#app',
     data: {
-    }
+        listItems:[1,2,3]
+    },
+    components: {
+        TestComponent: TestComponent,
+    }, 
 })
